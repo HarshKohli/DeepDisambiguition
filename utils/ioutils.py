b274f4bf84c39d01e0a1cae6667aa9c71e25b030
harsh <harshkohli@salesken.ai>
2020-02-18 17:17:18 +0530

    triplet_loss_training

--- utils/ioutils.py
+++ utils/ioutils.py
@@ -11,13 +11,17 @@ import numpy as np
 
 
 # Assumes schema - query entity_id cononical_name negative_samples
-def read_train_inputs(train_file, delimiter, max_len):
+def read_train_inputs(train_file, delimiter, max_len, max_negatives):
     f = open(train_file, 'r', encoding='utf8')
     unique_entity_map, train_samples = {}, []
     tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
     for line in f.readlines():
         info = line.split('\t')
         sentence, entity_id, canonical_name, negative_samples = info[0], info[1], info[2], info[3].split(delimiter)
+        if len(negative_samples) < max_negatives:
+            negative_samples = negative_samples + ['']*(max_negatives - len(negative_samples))
+        else:
+            negative_samples = negative_samples[:15]
         sentence_tokens = np.squeeze(tf.constant(tokenizer.encode(sentence, max_length=max_len, pad_to_max_length=True))[None, :])
         negative_tokens = []
         for negative_sample in negative_samples:
@@ -25,7 +29,6 @@ def read_train_inputs(train_file, delimiter, max_len):
         train_sample = TrainSample(sentence, entity_id, negative_samples, sentence_tokens, negative_tokens)
         train_samples.append(train_sample)
         if entity_id not in unique_entity_map:
-            # unique_entity_map[entity_id] = canonical_name
             entity_tokens = np.squeeze(tokenizer.encode(canonical_name, max_length=max_len, pad_to_max_length=True))
             unique_entity_map[entity_id] = EntityObj(entity_id, canonical_name, entity_tokens)
     random.shuffle(train_samples)
